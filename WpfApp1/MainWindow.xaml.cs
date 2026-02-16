0b2c9988b1356c1d777ecce1717b4ee448fd4ba2
Litt22 <zdanovicgleb@gmail.com>
2026-02-16 12:57:21 +0300

    реалезация mainwindow

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,17 +12,89 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Pages;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        public User CurrentUser { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            NavigateToMain();
+        }
+
+        public void NavigateToMain()
+        {
+            MainPage mainPage = new MainPage();
+            mainPage.SetCurrentUser(CurrentUser);
+            MainFrame.Navigate(mainPage);
+        }
+
+        public void NavigateToLogin()
+        {
+            LoginPage loginPage = new LoginPage();
+            MainFrame.Navigate(loginPage);
+        }
+
+        public void NavigateToLoginWithSession(int sessionId)
+        {
+            LoginPage loginPage = new LoginPage();
+            loginPage.ReturnSessionId = sessionId;
+            MainFrame.Navigate(loginPage);
+        }
+
+        public void NavigateToRegister()
+        {
+            RegisterPage registerPage = new RegisterPage();
+            MainFrame.Navigate(registerPage);
+        }
+
+        public void NavigateToMovie(int movieId, User user)
+        {
+            MoviePage moviePage = new MoviePage(movieId, user);
+            MainFrame.Navigate(moviePage);
+        }
+
+        public void NavigateToSession(int sessionId, int userId)
+        {
+            SessionPage sessionPage = new SessionPage(sessionId, userId);
+            MainFrame.Navigate(sessionPage);
+        }
+
+        public void NavigateToBooking(int sessionId, int userId, int seatId)
+        {
+            BookingPage bookingPage = new BookingPage(sessionId, userId, seatId);
+            MainFrame.Navigate(bookingPage);
+        }
+
+        public void NavigateToProfile(int userId)
+        {
+            ProfilePage profilePage = new ProfilePage(userId);
+            MainFrame.Navigate(profilePage);
+        }
+
+        public void CloseLoginPage(LoginPage loginPage)
+        {
+            CurrentUser = loginPage.LoggedUser;
+
+            if (loginPage.ReturnSessionId > 0)
+            {
+                NavigateToSession(loginPage.ReturnSessionId, CurrentUser.UserID);
+            }
+            else
+            {
+                NavigateToMain();
+            }
+        }
+
+        public void Logout()
+        {
+            CurrentUser = null;
+            NavigateToMain();
         }
     }
 }
