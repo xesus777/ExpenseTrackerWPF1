31a248d8b5edeffe3e828cad3c6944362775b89b
Litt22 <zdanovicgleb@gmail.com>
2026-05-17 18:20:41 +0300

    реалезация AuthPage - страница регистрации и входа

--- WpfApp1/Pages/AuthPage.xaml.cs
+++ WpfApp1/Pages/AuthPage.xaml.cs
@@ -15,14 +15,46 @@ using System.Windows.Shapes;
 
 namespace WpfApp1.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для AuthPage.xaml
-    /// </summary>
     public partial class AuthPage : Page
     {
         public AuthPage()
         {
             InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var user = Core.Context.Users.FirstOrDefault(u => u.Login == LoginBox.Text && u.PasswordHash == PasswordBox.Password);
+            if (user != null)
+            {
+                MainWindow.CurrentUserID = user.UserID;
+                (Application.Current.MainWindow as MainWindow).UpdateSidebar();
+                (Application.Current.MainWindow as MainWindow).MainFrame.Navigate(new CatalogPage());
+            }
+            else
+                ErrorText.Text = "Неверный логин или пароль";
+        }
+
+        private void RegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Core.Context.Users.Any(u => u.Login == RegLogin.Text))
+            {
+                ErrorText.Text = "Логин занят";
+                return;
+            }
+            var newUser = new Users
+            {
+                Login = RegLogin.Text,
+                PasswordHash = RegPassword.Password,
+                Email = RegEmail.Text,
+                DisplayName = RegDisplayName.Text,
+                RoleID = Core.Context.Roles.First(r => r.RoleName == "Читатель").RoleID,
+                IsFrozen = false,
+                CreatedAt = System.DateTime.Now
+            };
+            Core.Context.Users.Add(newUser);
+            Core.Context.SaveChanges();
+            ErrorText.Text = "Регистрация успешна, войдите";
         }
     }
 }
